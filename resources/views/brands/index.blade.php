7c6c879c8aa8ec2dfe5b57faedf16776a8154fd8
Felix Prima <felix.prima@binus.ac.id>
2023-05-02 00:52:39 +0700

    Added header on top 5 brands

--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="brand-container d-flex flex-row m-3 mt-5">
+
+    <h2 class="text-center mt-4 fw-bold">TOP 5 BRANDS</h2>
+    <div class="brand-container d-flex flex-row m-3 mt-3">
         @foreach ($brands as $brand)
             <div class="card text-center m-3" style="width: 18rem;">
                 <img src="https://www.pollock.com/wp-content/uploads/2019/10/shopping-bags-500x500-300x250.png" class="card-img-top" alt="...">
