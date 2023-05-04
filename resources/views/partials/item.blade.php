b785c39c26c85aab0c83c5c5dc5d2bf862b77898
Felix Prima <felix.prima@binus.ac.id>
2023-05-04 17:04:33 +0700

    Added alt

--- resources/views/partials/item.blade.php
+++ resources/views/partials/item.blade.php
@@ -8,16 +8,16 @@
                     <div class="card" style="width: 18rem;">
 
                         @if ($item->category->category_name == 'Bag')
-                            <img src="https://i.pinimg.com/originals/8a/3c/7f/8a3c7fbd4d9532a244ef3d5027d6e4c6.jpg" class="card-img-top m-3" alt="..." style="width:15vw; height:30vh">
+                            <img src="https://i.pinimg.com/originals/8a/3c/7f/8a3c7fbd4d9532a244ef3d5027d6e4c6.jpg" class="card-img-top m-3" alt="{{ $item->category->category_name }}" style="width:15vw; height:30vh">
                         
                         @elseif ($item->category->category_name == 'Shirt')
-                            <img src="https://uxwing.com/wp-content/themes/uxwing/download/clothes-and-accessories/men-t-shirt-icon.png" alt="" class="card-img-top m-3" style="width:15vw; height:30vh">
+                            <img src="https://uxwing.com/wp-content/themes/uxwing/download/clothes-and-accessories/men-t-shirt-icon.png" alt="{{ $item->category->category_name }}" class="card-img-top m-3" style="width:15vw; height:30vh">
 
                         @elseif ($item->category->category_name == 'Shoes')
-                            <img src="https://cdn-icons-png.flaticon.com/512/5499/5499206.png" alt="" class="card-img-top m-3" style="width:15vw; height:30vh">
+                            <img src="https://cdn-icons-png.flaticon.com/512/5499/5499206.png" alt="{{ $item->category->category_name }}" class="card-img-top m-3" style="width:15vw; height:30vh">
 
                         @elseif ($item->category->category_name == 'Dress')
-                            <img src="https://cdn-icons-png.flaticon.com/512/1059/1059053.png" alt="" class="card-img-top m-3" style="width:15vw; height:30vh">
+                            <img src="https://cdn-icons-png.flaticon.com/512/1059/1059053.png" alt="{{ $item->category->category_name }}" class="card-img-top m-3" style="width:15vw; height:30vh">
 
                         @endif
 
