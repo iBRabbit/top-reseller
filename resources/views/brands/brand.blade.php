8fa16133aeea7e498e014d8d6df8329bcee79ce4
Felix Prima <felix.prima@binus.ac.id>
2023-05-05 13:21:28 +0700

    Final Commit

--- resources/views/brands/brand.blade.php
+++ resources/views/brands/brand.blade.php
@@ -18,19 +18,5 @@
 
     <h3 class="mt-4">{{ $brand->brand_name }}'s items</h3>
 
-    <div class="brand-items-container">
-        <div class="brand-items-card-container d-flex flex-row me-3 mb-3">
-            @foreach ($items as $item)
-                <div class="card" style="width: 18rem;">
-                    <img src="https://www.pollock.com/wp-content/uploads/2019/10/shopping-bags-500x500-300x250.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $item->item_name }}</h5>
-                        <p class="card-text" style="color:orange">{{ $item->brand->brand_name}}</p>
-                        <p class="card-text">{{ 'Rp' . number_format($item->price)}}</p>
-                        <a href="/item/{{ $item->id }}" class="btn btn-primary">Detail</a>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
+    @include('partials.item')
 @endsection
